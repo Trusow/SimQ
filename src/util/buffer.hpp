9e15b788428181ed0208b0df6c5446e9b7a8c46c
Трусов Сергей <tserg@hub.sknt.ru>
2023-03-23 21:00:52 +0300

    buffer: корректная запись и чтение данных

--- src/util/buffer.hpp
+++ src/util/buffer.hpp
@@ -20,6 +20,14 @@ namespace simq::util {
             const unsigned int MIN_FILE_SIZE = 50 * LENGTH_PACKET_ON_DISK;
             const unsigned int SIZE_ITEM_PACKET = 10'000;
 
+            struct WRData {
+                unsigned int startPartOffset;
+                unsigned int startOffset;
+                unsigned int startSize;
+                unsigned int sizes;
+                unsigned int endSize;
+            };
+
             struct Item {
                 unsigned int lengthEnd;
                 unsigned int length;
@@ -48,7 +56,23 @@ namespace simq::util {
             void initFileSize();
             void initItems();
             void expandItems();
+            void expandFile();
             void wait( std::atomic_uint &atom );
+
+            void _calculateWriteData(
+                unsigned int fullLength,
+                unsigned int partSize,
+                unsigned int addLength,
+                WRData &data
+            );
+            void _calculateReadData(
+                unsigned int fullLength,
+                unsigned int partSize,
+                unsigned int offset,
+                unsigned int readLength,
+                WRData &data
+            );
+
             void _writeBuffer( Item *item, const char *data, unsigned int length );
             void _writeFile( Item *item, const char *data, unsigned int length );
             char *_readBuffer( Item *item, char *data, unsigned int length, unsigned int offset );
@@ -115,6 +139,34 @@ namespace simq::util {
         }
     }
 
+    unsigned int Buffer::allocateOnDisk() {
+        util::LockAtomic lockAtomicGroup( countItemsWrited );
+        std::lock_guard<std::shared_timed_mutex> lock( mItems );
+
+        auto id = getUniqID();
+
+        auto offsetPacket = id / SIZE_ITEM_PACKET;
+        auto offset = id - offsetPacket * SIZE_ITEM_PACKET;
+
+        auto item = new Item{};
+        item->packetSize = LENGTH_PACKET_ON_DISK;
+        item->length++;
+        item->itemsDisk = new unsigned long int[1]{};
+
+        items[offsetPacket][offset] = item;
+
+        std::lock_guard<std::mutex> lockFile( mFile );
+
+        if( freeFileOffsets.empty() ) {
+            expandFile();
+        }
+
+        item->itemsDisk[0] = freeFileOffsets.front();
+        freeFileOffsets.pop();
+
+        return id;
+    }
+
     unsigned int Buffer::allocate( unsigned int packetSize ) {
         if( packetSize == 0 ) {
             throw util::Error::WRONG_PARAM;
@@ -176,6 +228,12 @@ namespace simq::util {
             }
             delete[] item->buffer;
         } else {
+            std::lock_guard<std::mutex> lockFile( mFile );
+
+            for( int i = 0; i < item->length; i++ ) {
+                freeFileOffsets.push( item->itemsDisk[i] );
+            }
+
             delete[] item->itemsDisk;
         }
 
@@ -184,39 +242,170 @@ namespace simq::util {
         freeUniqID( id );
     }
 
+    void Buffer::_calculateWriteData(
+        unsigned int fullLength,
+        unsigned int partSize,
+        unsigned int addLength,
+        WRData &data
+    ) {
+        unsigned int ceilFullLength = fullLength / partSize * partSize + ( fullLength % partSize == 0 ? 0 : partSize );
+     
+        if( ceilFullLength == 0 ) {
+            ceilFullLength = partSize;
+        }
+     
+        data.startPartOffset = ceilFullLength / partSize;
+        if( data.startPartOffset != 0 ) {
+            data.startPartOffset--;
+        }
+        data.startOffset = fullLength - data.startPartOffset * partSize;
+        if( data.startOffset == partSize ) {
+            data.startOffset = 0;
+        }
+     
+        if( fullLength + addLength < ceilFullLength ) {
+            data.sizes = 0;
+            data.endSize = 0;
+            data.startSize = addLength;
+        } else {
+            data.startSize = ceilFullLength - fullLength;
+            addLength -= data.startSize;
+            data.sizes = addLength / partSize;
+            addLength -= data.sizes * partSize;
+            data.endSize = addLength;
+            if( data.endSize ) {
+                data.sizes++;
+            }
+        }
+    }
+     
+    void Buffer::_calculateReadData(
+        unsigned int fullLength,
+        unsigned int partSize,
+        unsigned int offset,
+        unsigned int readLength,
+        WRData &data
+    ) {
+        if( offset + readLength > fullLength ) {
+            return;
+        }
+     
+        data.startPartOffset = offset / partSize;
+        data.startOffset = offset - data.startPartOffset * partSize;
+     
+        if( partSize - data.startOffset > readLength ) {
+            data.startSize = readLength;
+            data.sizes = 0;
+            data.endSize = 0;
+        } else {
+            data.startSize = partSize - data.startOffset;
+            readLength -= data.startSize;
+            data.sizes = readLength / partSize;
+            data.endSize = readLength - data.sizes * partSize;
+            if( data.endSize ) {
+                data.sizes++;
+            }
+        }
+    }
+
     char *Buffer::_readFile( Item *item, char *data, unsigned int length, unsigned int offset ) {
-        return nullptr;
+        std::lock_guard<std::mutex> lock( mFile );
+
+        const unsigned int packetSize = item->packetSize;
+        WRData wrData;
+        unsigned int fullLength = item->length == 1 ? item->lengthEnd : item->length * packetSize + item->lengthEnd;
+        _calculateReadData( fullLength, packetSize, offset, length, wrData );
+
+        if( wrData.startSize ) {
+            FS::readFile(
+                file,
+                item->itemsDisk[wrData.startPartOffset] * packetSize + wrData.startOffset,
+                wrData.startSize,
+                (void *)data
+            );
+        }
+
+        for( unsigned int i = 0; i < wrData.sizes; i++ ) {
+            wrData.startPartOffset++;
+            FS::readFile(
+                file,
+                item->itemsDisk[wrData.startPartOffset] * packetSize,
+                i == wrData.sizes - 1 ? wrData.startSize : packetSize,
+                (void *)&data[wrData.startSize + i * packetSize]
+            );
+        }
+
+        return data;
+    }
+
+    void Buffer::_writeFile( Item *item, const char *data, unsigned int length ) {
+        std::lock_guard<std::mutex> lock( mFile );
+
+
+        const unsigned int packetSize = LENGTH_PACKET_ON_DISK;
+        WRData wrData;
+        unsigned int fullLength = item->length == 1 ? item->lengthEnd : item->length * packetSize + item->lengthEnd;
+        _calculateWriteData( fullLength, packetSize, length, wrData );
+
+        if( wrData.startSize ) {
+            FS::writeFile(
+                file,
+                item->itemsDisk[wrData.startPartOffset] * packetSize + wrData.startOffset,
+                wrData.startSize,
+                (void *)data
+            );
+        }
+
+        if( wrData.sizes ) {
+            auto tmpItemsDisk = new unsigned long int[item->length + wrData.sizes]{};
+            memcpy( tmpItemsDisk, item->itemsDisk, item->length * sizeof( unsigned long int ) );
+
+            delete[] item->itemsDisk;
+            item->itemsDisk = tmpItemsDisk;
+            item->lengthEnd = wrData.endSize;
+        } else {
+            item->lengthEnd += wrData.startSize;
+        }
+
+        for( unsigned int i = 0; i < wrData.sizes; i++ ) {
+            if( freeFileOffsets.empty() ) {
+                expandFile();
+            }
+            wrData.startPartOffset++;
+            item->length++;
+            item->itemsDisk[wrData.startPartOffset] = freeFileOffsets.front();
+            freeFileOffsets.pop();
+
+            FS::writeFile(
+                file,
+                item->itemsDisk[wrData.startPartOffset] * packetSize,
+                i == wrData.sizes - 1 ? wrData.endSize : packetSize,
+                (void *)&data[wrData.startSize + i * packetSize]
+            );
+        }
     }
 
     char *Buffer::_readBuffer( Item *item, char *data, unsigned int length, unsigned int offset ) {
-
-        const unsigned int fullLength = item->packetSize * ( item->length - 1 ) + item->lengthEnd;
-
-        if( offset + length > fullLength ) {
-            return nullptr;
-        }
-
         const unsigned int packetSize = item->packetSize;
+        WRData wrData;
+        unsigned int fullLength = item->length == 1 ? item->lengthEnd : item->length * packetSize + item->lengthEnd;
+        _calculateReadData( fullLength, packetSize, offset, length, wrData );
 
-        unsigned int pageStart = offset / packetSize;
-        unsigned int sizeStart = offset - pageStart * packetSize;
-        unsigned int acc = 0;
-
-        memcpy( data, &item->buffer[pageStart][sizeStart], packetSize - sizeStart );
-        acc += packetSize - sizeStart;
-
-        unsigned int sizes = ( length - ( packetSize - sizeStart ) ) / packetSize;
-        for( unsigned int i = 0; i < sizes; i++ ) {
-            pageStart++;
-            memcpy( &data[acc], &item->buffer[pageStart][0], packetSize );
-            acc += packetSize;
+        if( wrData.startSize ) {
+            memcpy(
+                data,
+                &item->buffer[wrData.startPartOffset][wrData.startOffset],
+                wrData.startSize
+            );
         }
 
-        const unsigned int residue = sizes * packetSize + ( packetSize - sizeStart );
-
-        if( residue < length ) {
-            pageStart++;
-            memcpy( &data[acc], &item->buffer[pageStart][0], length - residue );
+        for( unsigned int i = 0; i < wrData.sizes; i++ ) {
+            wrData.startPartOffset++;
+            memcpy(
+                &data[wrData.startSize + i * packetSize],
+                &item->buffer[wrData.startPartOffset][0],
+                i == wrData.sizes - 1 ? wrData.endSize : packetSize
+            );
         }
 
         return data;
@@ -224,53 +413,43 @@ namespace simq::util {
 
     void Buffer::_writeBuffer( Item *item, const char *data, unsigned int length ) {
         const unsigned int packetSize = item->packetSize;
-        const unsigned int writeSize = item->lengthEnd;
-     
-        unsigned int sizes = 0;
-        unsigned int prevFreeSize = 0;
-        unsigned int afterFreeSize = 0;
+        WRData wrData;
+        unsigned int fullLength = item->length == 1 ? item->lengthEnd : item->length * packetSize + item->lengthEnd;
+        _calculateWriteData( fullLength, packetSize, length, wrData );
 
-        if( writeSize + length <= packetSize ) {
-            prevFreeSize = length;
-        } else {
-            prevFreeSize = packetSize - writeSize;
-            sizes = ( length - prevFreeSize ) / packetSize + ( ( length - prevFreeSize ) % packetSize != 0 );
-            afterFreeSize = length - prevFreeSize - ( sizes - 1 ) * packetSize;
-        }
-
-        if( prevFreeSize ) {
-            memcpy( &item->buffer[item->length-1][item->lengthEnd], data, prevFreeSize );
-            item->lengthEnd += prevFreeSize;
+        if( wrData.startSize ) {
+            memcpy(
+                &item->buffer[wrData.startPartOffset][wrData.startOffset],
+                data,
+                wrData.startSize
+            );
         }
 
 
-        if( sizes ) {
-            auto tmpBuffer = new char*[sizes+item->length]{};
+        if( wrData.sizes ) {
+            auto tmpBuffer = new char*[wrData.sizes+item->length]{};
             memcpy( tmpBuffer, item->buffer, item->length * sizeof( char * ) );
 
             delete[] item->buffer;
             item->buffer = tmpBuffer;
-
-            for( unsigned int i = 0; i < sizes-1; i++ ) {
-                item->length++;
-                item->buffer[item->length-1] = new char[packetSize]{};
-                memcpy( &item->buffer[item->length-1][0], &data[prevFreeSize + ( i * packetSize )], packetSize );
-            }
+            item->lengthEnd = wrData.endSize;
+        } else {
+            item->lengthEnd = wrData.startSize;
         }
 
-
-        if( afterFreeSize ) {
+        for( unsigned int i = 0; i < wrData.sizes; i++ ) {
+            wrData.startPartOffset++;
             item->length++;
-            item->buffer[item->length-1] = new char[packetSize]{};
-            item->lengthEnd = afterFreeSize;
-            memcpy( &item->buffer[item->length-1][0], &data[length-afterFreeSize], afterFreeSize );
+            item->buffer[wrData.startPartOffset] = new char[packetSize]{};
+            memcpy(
+                &item->buffer[wrData.startPartOffset][0],
+                &data[wrData.startSize + packetSize * i],
+                i == wrData.sizes - 1 ? wrData.endSize : packetSize
+            );
         }
 
     }
 
-    void Buffer::_writeFile( Item *item, const char *data, unsigned int length ) {
-        std::lock_guard<std::mutex> lock( mFile );
-    }
 
     void Buffer::write( unsigned int id, const char *data, unsigned int length ) {
         wait( countItemsWrited );
@@ -322,7 +501,7 @@ namespace simq::util {
     unsigned int Buffer::getUniqID() {
 
         if( !freeIDs.empty() ) {
-            auto id = freeIDs.back();
+            auto id = freeIDs.front();
             freeIDs.pop();
             return id;
         } else {
@@ -351,6 +530,14 @@ namespace simq::util {
         countItemsPackets++;
         items = new Item **[countItemsPackets]{};
         items[0] = new Item*[SIZE_ITEM_PACKET]{};
+    }
+
+    void Buffer::expandFile() {
+        fileOffset += MIN_FILE_SIZE / LENGTH_PACKET_ON_DISK;
+        FS::expandFile( file, MIN_FILE_SIZE );
+        for( unsigned int i = fileOffset; i < fileOffset + MIN_FILE_SIZE / LENGTH_PACKET_ON_DISK; i++ ) {
+            freeFileOffsets.push( i );
+        }
     }
 
     void Buffer::initFileSize() {
