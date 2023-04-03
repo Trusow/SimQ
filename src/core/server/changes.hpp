bf745428a91fbc626a29c04f221a3df161a648d7
Трусов Сергей <tserg@hub.sknt.ru>
2023-04-03 19:03:25 +0300

    changes: переосмысление

--- src/core/server/changes.hpp
+++ src/core/server/changes.hpp
@@ -1,40 +1,66 @@
 #ifndef SIMQ_CORE_SERVER_CHANGES
 #define SIMQ_CORE_SERVER_CHANGES
 
-#include <string>
 #include "../../crypto/hash.hpp"
+#include "../../util/types.h"
+#include "../../util/file.hpp"
+#include "../../util/uuid.hpp"
+#include <list>
 
 namespace simq::core::server {
     class Changes {
         public:
-            Changes( const char *path );
-            ~Changes();
-
             enum Type {
                 CREATE_GROUP,
                 REMOVE_GROUP,
             };
+        private:
+            const char *defPath = "changes";
 
-            struct Change {
+            struct ChangeFile {
                 Type type;
-                unsigned int valuesChar[8][32];
-                unsigned int valuesCharLength[8];
-                unsigned int valuesUInt[8];
-                unsigned int customDataLength;
+                unsigned int values[16];
+                unsigned int valuesLength;
+            };
+        public:
+
+            struct Change: ChangeFile {
+                void *data;
             };
 
-            void addGroup( const char *group, unsigned char password[crypto::HASH_LENGTH], Change &change );
-            void removeGroup( const char *group, Change &change );
+        private:
 
-            bool isAddGroup( Change &change );
-            void getParamsAddGroup( std::string &group, unsigned char password[crypto::HASH_LENGTH] );
+            struct ChangeDisk {
+                Change *change;
+                char uuid[util::UUID::LENGTH];
+            };
 
-            bool isRemoveGroup( Change &change );
-            void getParamsRemoveGroup( std::string &group );
+            std::list<Change *> listMemory;
+            std::list<ChangeDisk> listDisk;
 
-            void push( Change &change );
-            void pushDefered( Change &change );
-            void pop( Change &change );
+        public:
+            Changes( const char *path );
+            ~Changes();
+
+
+
+            Change *addGroup( const char *group, unsigned char password[crypto::HASH_LENGTH] );
+            Change *removeGroup( const char *group );
+
+            bool isAddGroup( Change *change );
+            bool isRemoveGroup( Change *change );
+
+            const char *getGroup( Change *change );
+            const char *getChannel( Change *change );
+            const char *getLogin( Change *change );
+            const util::Types::ChannelSettings *getChannelSettings( Change *change );
+            const unsigned char *getPassword( Change *change );
+
+            void push( Change *change );
+            void pushDefered( Change *change );
+            Change *pop();
+
+            void free( Change *change );
     };
 }
 
