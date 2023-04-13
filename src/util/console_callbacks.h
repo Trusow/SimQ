668fb6b110c1c59cc0e406cbfc2543b0710998df
Трусов Сергей <tserg@hub.sknt.ru>
2023-04-13 20:21:10 +0300

    console_callbacks: fish

---
+++ src/util/console_callbacks.h
@@ -0,0 +1,16 @@
+#ifndef SIMQ_UTIL_CONSOLE_CALLBACKS
+#define SIMQ_UTIL_CONSOLE_CALLBACKS
+
+#include <vector>
+#include <string>
+
+namespace simq::util {
+    class ConsoleCallbacks {
+        public:
+            virtual void prompt( bool ) = 0;
+            virtual void inputPassword( const char * ) = 0;
+            virtual void input( std::vector<std::string> & ) = 0;
+    };
+}
+
+#endif
