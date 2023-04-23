6febec4f5c8e27e033d37cd9d80c4a6c44deb1bf
Трусов Сергей <tserg@hub.sknt.ru>
2023-04-23 15:34:58 +0300

    string: регистронезависимый поиск

--- src/util/string.hpp
+++ src/util/string.hpp
@@ -2,12 +2,14 @@
 #define SIMQ_UTIL_STRING
 
 #include <string.h>
+#include <ctype.h>
 
 namespace simq::util {
     class String {
         public:
             static void free( char *str );
             static char *copy( const char *src );
+            static int find( const char *src, const char *target );
     };
 
     void String::free( char *str ) {
@@ -26,6 +28,30 @@ namespace simq::util {
         auto target = new char[l+1]{};
         memcpy( target, src, l );
         return target;
+    }
+
+    int String::find( const char *line, const char *query ) {
+        auto lLine = strlen( line );
+        auto lQuery = strlen( query );
+        auto _line = new char[lLine+1]{};
+        auto _query = new char[lQuery+1]{};
+
+        for( unsigned int i = 0; line[i] != 0; i++ ) {
+            _line[i] = toupper( line[i] );
+        }
+
+        for( unsigned int i = 0; query[i] != 0; i++ ) {
+            _query[i] = toupper( query[i] );
+        }
+
+        auto res = strstr( _line, _query );
+
+        auto offset = res != NULL ? res - _line : -1;
+
+        delete[] _line;
+        delete[] _query;
+
+        return offset;
     }
 }
 
