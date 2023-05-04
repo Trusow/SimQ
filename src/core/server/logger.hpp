694b690e3b6f48f03b3ee0eb32cf9af491ebccef
Трусов Сергей <tserg@hub.sknt.ru>
2023-05-04 18:51:32 +0300

    server: logger: fish

---
+++ src/core/server/logger.hpp
@@ -0,0 +1,15 @@
+#ifndef SIMQ_CORE_SERVER_LOGGER
+#define SIMQ_CORE_SERVER_LOGGER
+
+#include <list>
+#include <string>
+#include "../../util/logger.hpp"
+
+namespace simq::core::server {
+    class Logger {
+        public:
+    };
+}
+
+#endif
+ 
