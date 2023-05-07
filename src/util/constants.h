3611a37b59d4522d5237d7946b6df8d5e1d0935f
Трусов Сергей <tserg@hub.sknt.ru>
2023-05-07 16:58:48 +0300

    constants: вынос путей в константы

--- src/util/constants.h
+++ src/util/constants.h
@@ -3,6 +3,16 @@
 
 namespace simq::util::constants {
     inline const unsigned int MESSAGE_PACKET_SIZE = 4096;
+
+    inline const char *PATH_DIR_GROUPS = "groups";
+    inline const char *PATH_DIR_CHANNELS = "channels";
+    inline const char *PATH_DIR_CONSUMERS = "consumers";
+    inline const char *PATH_DIR_PRODUCERS = "producers";
+    inline const char *PATH_FILE_PASSWORD = "password";
+    inline const char *PATH_FILE_CHANNEL_LIMIT_MESSAGES = "limit-messages";
+    inline const char *PATH_DIR_SETTINGS = "settings";
+    inline const char *PATH_FILE_SETTINGS = "settings";
+    inline const char *PATH_DIR_CHANGES = "changes";
 }
 
 #endif
