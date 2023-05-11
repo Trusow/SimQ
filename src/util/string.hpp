9cf78f5b9c7aa6b435a45536ebd23a16c5c7f0e9
Трусов Сергей <tserg@hub.sknt.ru>
2023-05-11 22:29:25 +0300

    string: unique_ptr & выпилка ненужных методов

--- src/util/string.hpp
+++ src/util/string.hpp
@@ -3,38 +3,19 @@
 
 #include <string.h>
 #include <ctype.h>
+#include <memory>
 
 namespace simq::util {
     class String {
         public:
-            static void free( char *str );
-            static char *copy( const char *src );
             static int find( const char *src, const char *target );
     };
-
-    void String::free( char *str ) {
-        if( str != nullptr ) {
-            delete[] str;
-            str = nullptr;
-        }
-    }
-
-    char *String::copy( const char *src ) {
-        if( src == nullptr ) {
-            return nullptr;
-        }
-
-        auto l = strlen( src );
-        auto target = new char[l+1]{};
-        memcpy( target, src, l );
-        return target;
-    }
 
     int String::find( const char *line, const char *query ) {
         auto lLine = strlen( line );
         auto lQuery = strlen( query );
-        auto _line = new char[lLine+1]{};
-        auto _query = new char[lQuery+1]{};
+        auto _line = std::make_unique<char[]>( lLine+1 );
+        auto _query = std::make_unique<char[]>( lQuery+1 );
 
         for( unsigned int i = 0; line[i] != 0; i++ ) {
             _line[i] = toupper( line[i] );
@@ -44,12 +25,9 @@ namespace simq::util {
             _query[i] = toupper( query[i] );
         }
 
-        auto res = strstr( _line, _query );
+        auto res = strstr( _line.get(), _query.get() );
 
-        auto offset = res != NULL ? res - _line : -1;
-
-        delete[] _line;
-        delete[] _query;
+        auto offset = res != NULL ? res - _line.get() : -1;
 
         return offset;
     }
