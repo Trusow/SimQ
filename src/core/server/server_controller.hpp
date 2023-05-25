586b23d81c42d0f797cf9767899d5fa81af91fd5
Трусов Сергей <tserg@hub.sknt.ru>
2023-05-25 19:30:03 +0300

    server_controller: resetRecv после отправки данных

--- src/core/server/server_controller.hpp
+++ src/core/server/server_controller.hpp
@@ -303,6 +303,8 @@ namespace simq::core::server {
 
         if( !Protocol::send( fd, packet ) ) return;
 
+        Protocol::resetRecv( packet );
+
         switch( sess->fsm ) {
             case FSM_COMMON_SEND_CONFIRM_SECURE:
                 sess->fsm = FSM_COMMON_RECV_CMD_GET_VERSION;
