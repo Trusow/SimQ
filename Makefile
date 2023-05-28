96e60f42f5d30857ab38b14f9d653c0777ac5ce4
Трусов Сергей <tserg@hub.sknt.ru>
2023-05-28 12:35:45 +0300

    build: сборка simq-server

--- Makefile
+++ Makefile
@@ -1,18 +1,4 @@
-client:
-	g++ client.cpp \
+simq-server:
+	g++ simq-server.cpp \
 	\
-	src/tui/button.cpp \
-	src/tui/input.cpp \
-	src/tui/dialog.cpp \
-	src/tui/dialog_auth.cpp \
-	src/tui/dialog_groups.cpp \
-	src/tui/dialog_settings.cpp \
-	src/tui/dialog_groups_test.cpp \
-	src/tui/main.cpp \
-	\
-	-ldl -pthread -L/usr/lib/ -lfinal -lncursesw -static -std=c++2a -s -O3 -Wl,--whole-archive -lpthread -Wl,--no-whole-archive -o ./bin/client
-#	-lcrypto -ldl -pthread -L/usr/lib/ -lfinal -lncursesw -static -std=c++2a -s -O3 -Wl,--whole-archive -lpthread -Wl,--no-whole-archive -o ./bin/client
-test:
-	g++ test.cpp \
-	\
-	-lcrypto -ldl -pthread -L/usr/lib/ -lfinal -lncursesw -std=c++2a -s -O3 -Wl,--whole-archive -lpthread -Wl,--no-whole-archive -o ./bin/test
+	-lcrypto -ldl -pthread -L/usr/lib/ -static -std=c++2a -s -O3 -Wl,--whole-archive -lpthread -Wl,--no-whole-archive -o ./bin/simq-server
---
+++ simq-server.cpp
@@ -0,0 +1,103 @@
+#include "src/core/server/cli/manager.hpp"
+#include "src/core/server/logger.hpp"
+#include "src/util/error.h"
+#include "src/core/server/cli_controller.hpp"
+#include "src/core/server/initialization.hpp"
+#include "src/core/server/server/manager.hpp"
+#include "src/core/server/server_controller.hpp"
+#include <thread>
+#include <list>
+#include <string>
+#include <atomic>
+
+void startManager( const char *path = "." ) {
+    simq::core::server::Store store( path );
+    simq::core::server::Changes changes( path );
+    simq::core::server::CLIController controller( &store, &changes );
+
+    simq::core::server::CLI::Manager manger( &controller );
+
+}
+
+std::atomic_bool isPassedStartServer{ true };
+
+void startServer(
+    simq::core::server::Store *store,
+    simq::core::server::Access *access,
+    simq::core::server::Changes *changes,
+    simq::core::server::q::Manager *q
+) {
+    if( !isPassedStartServer ) {
+        return;
+    }
+
+
+    try {
+        simq::core::server::server::Manager server( store->getPort() );
+        simq::core::server::ServerController controller( store, access, changes, q );
+
+        server.bindController( &controller );
+
+        std::list<simq::core::server::Logger::Detail> list;
+        simq::core::server::Logger::success( simq::core::server::Logger::OP_START_SERVER, 0, list );
+
+        server.run();
+    } catch( ... ) {
+        std::list<simq::core::server::Logger::Detail> list;
+        simq::core::server::Logger::fail(
+            simq::core::server::Logger::OP_START_SERVER,
+            simq::util::Error::SOCKET,
+            0,
+            list
+        );
+
+        isPassedStartServer = false;
+    }
+}
+
+void startInit( const char *path = "." ) {
+    simq::core::server::Access access;
+    simq::core::server::q::Manager q;
+
+    simq::core::server::Initialization ini( path, access, q );
+
+    ini.pollChanges();
+
+    auto changes = ini.getChanges();
+    auto store = ini.getStore();
+
+    for( unsigned int i = 0; i < store->getCountThreads(); i++ ) {
+        std::thread t( startServer, store, &access, changes, &q );
+        t.detach();
+    }
+
+    std::chrono::milliseconds ts( 50 );
+
+    while( true ) {
+        if( !isPassedStartServer ) {
+            break;
+        }
+        ini.pollChanges();
+        std::this_thread::sleep_for( ts );
+    }
+
+}
+
+int main( int argc, char *argv[] ) {
+    bool isManager = false;
+    std::string path = ".";
+
+    for( unsigned int i = 1; i < argc; i++ ) {
+        std::string val = std::string( argv[i] );
+
+        if( val == "manager" ) {
+            isManager = true;
+        }
+    }
+
+    if( isManager ) {
+        startManager( path.c_str() );
+    } else {
+        startInit( path.c_str() );
+    }
+}
